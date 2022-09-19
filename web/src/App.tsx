75762a8cfad0dd42e9989beb9a73221437112bb8
Fernando T. Barros <fernando@deltasi.com.br>
2022-09-18 23:49:52 -0300

    Finalizando front-end em React.js

---
+++ web/src/App.tsx
@@ -0,0 +1,62 @@
+import "./styles/main.css";
+
+import * as Dialog from "@radix-ui/react-dialog";
+import { useEffect, useState } from "react";
+import logoImg from "./assets/logo-nlw-esports.svg";
+import { CreateAdBanner } from "./componentes/CreateAdBanner";
+import { CreateAdModal } from "./componentes/CreateAdModal";
+import { GameBanner } from "./componentes/GameBanner";
+import axios from "axios";
+
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  };
+}
+
+function App() {
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    axios("http://localhost:3333/games").then((response) =>
+      setGames(response.data)
+    );
+  }, []);
+
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="Logo do NLW eSports" />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          duo
+        </span>{" "}
+        está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        {games.map((game) => {
+          return (
+            <GameBanner
+              key={game.id}
+              title={game.title}
+              bannerUrl={game.bannerUrl}
+              adsCount={game._count.ads}
+            />
+          );
+        })}
+      </div>
+
+      <Dialog.Root>
+        <CreateAdBanner />
+        <CreateAdModal />
+      </Dialog.Root>
+    </div>
+  );
+}
+
+export default App;
